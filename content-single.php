a86676dc2fbe491db936feb725bb295bed82efcb
Luke Carbis <luke.carbis@x-team.com>
2014-09-18 17:11:38 +1000

    Feature image implementation on single post

--- content-single.php
+++ content-single.php
@@ -5,12 +5,21 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	<header class="<?php esc_attr_e( has_post_thumbnail() ? 'entry-header has-thumbnail' : 'entry-header' ) ?>">
+
+		<?php the_title( '<h1 class="entry-title"><span>', '</span></h1>' ); ?>
+
+		<?php
+		if ( has_post_thumbnail() ) {
+			$thumbnail      = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' );
+			printf( '<div class="entry-thumbnail" style="background-image: url(%s);"></div>', esc_attr( $thumbnail[0] ) ); //xss ok
+		}
+		?>
 
 		<div class="entry-meta">
 			<?php stock_posted_on(); ?>
 		</div><!-- .entry-meta -->
+
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
--- content.php
+++ content.php
@@ -19,7 +19,7 @@
 		<?php
 			/* translators: %s: Name of current post */
 			the_content( sprintf(
-				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'stock' ), 
+				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'stock' ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			) );
 		?>
