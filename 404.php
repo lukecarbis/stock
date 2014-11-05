9b181edbedd59991c7e9267220b3481a63616a66
Luke Carbis <luke.carbis@x-team.com>
2014-11-05 13:13:51 +1000

    Improve display of 404 page

--- 404.php
+++ 404.php
@@ -18,7 +18,7 @@ get_header(); ?>
 				<div class="page-content">
 					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'stock' ); ?></p>
 
-					<?php get_search_form(); ?>
+					<?php the_widget( 'WP_Widget_Search' ); ?>
 
 					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
